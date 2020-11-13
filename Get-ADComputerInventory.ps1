cd68c1fd0e7c70ae9416b267b1223373e46a31f0
Daniel Sajdyk <kontakt@sajdyk.pl>
2020-11-13 22:29:15 +0100

    Skrypt pobiera teraz uzytkownikow grupy lokalnej Administratorzy (jest to robione poprzez SID grupy) oraz adres IP i sposob jego ustawienia w serwerze \(DHCP, lub inaczej\).

--- Get-ADComputerInventory.ps1
+++ Get-ADComputerInventory.ps1
@@ -63,32 +63,39 @@ if ($TRUE -eq $computersToScan) {
     if ($TRUE -eq $sessions) {
         $results = Invoke-Command -Session $sessions -ScriptBlock {
 
+            # Code for PowerShell with major version greater than 5
             if (($PSVersionTable.PSVersion.Major) -ge 5) {
                 $Win32_ComputerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -property *
                 $Win32_Processor = Get-CimInstance -ClassName Win32_Processor -property *
                 $Win32_SystemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure -property *
                 $Win32_DiskDrive = Get-CimInstance -ClassName Win32_DiskDrive -property *
                 $OS = ((Get-CimInstance Win32_OperatingSystem -Property *).Name.Split("|") | Select-Object -First 1)
+                $IPAddress = (Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {($_)} | Where-Object {$_.AddressFamily -like "IPv4"};
                 
                 $tablica = @{}
                 
                 $tablica.add("ScanDate",             (Get-Date -DisplayHint Date)) #Get-Date -Format {dd.MM.yyyy}
-                $tablica.add("PhysicalHostname",     ([System.Net.Dns]::GetHostByName($env:computerName)).HOSTNAME)
+                $tablica.add("PhysicalHostname",     ([System.Net.Dns]::GetHostByName($env:computerName)).HOSTNAME) #Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property {$_.Name + "." + $_.Domain}
                 $tablica.add("Manufacturer",         $Win32_ComputerSystem.Manufacturer)
                 $tablica.add("Model",                $Win32_ComputerSystem.Model)
                 $tablica.add("ComputerSerialNumber", $Win32_SystemEnclosure.SerialNumber)
-                $tablica.add("ProcessorName",        $Win32_Processor.Name) #Get-Ciminstance -Query 'SELECT Name FROM Win32_Processor'
+                $tablica.add("ProcessorName",        (($Win32_Processor.Name) | Get-Unique)) #Get-Ciminstance -Query 'SELECT Name FROM Win32_Processor'
+                $tablica.add("NrOfProcessors",       ($Win32_Processor.Name).Count)
                 $tablica.add("RAM",                  ([math]::Round(($Win32_ComputerSystem.TotalPhysicalMemory / 1GB),2)))
                 $tablica.add("DiskDrive",            ($Win32_DiskDrive.Size | ForEach-Object {[math]::Round(($_ / 1GB),2)}) )
                 #$tablica.add("HDDPartitions",        (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3").Size   )
                 #HDDPartitions = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3").Size | ForEach-Object {[math]::Round(($_ / 1GB),2)};
                 $tablica.add("OS",                   $OS)
-                $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor) + "." + ($PSVersionTable.PSVersion.Patch)))
+                #$tablica.add("LocalAccounts",       (get-localuser | foreach {if($_.Enabled -eq $true){$_.Name + "`(A`)"}else{$_.Name +"`(D`)"}}))
+                $tablica.add("LocalAdministrators",  (Get-LocalGroupMember -SID S-1-5-32-544))
+                $tablica.add("IPAddress",            $IPAddress.IPAddress)
+                $tablica.add("SuffixOrigin",         $IPAddress.SuffixOrigin)
+                $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor)))
                 
                 new-object -Type psobject -Property $tablica
             }
 
-            # Code for PowerShell with version less than 5
+            # Code for PowerShell with major version less than 5
             else {
                 $Win32_ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem
                 $Win32_Processor = Get-WmiObject -Class Win32_Processor
@@ -103,11 +110,15 @@ if ($TRUE -eq $computersToScan) {
                 $tablica.add("Model",               $Win32_ComputerSystem.Model)
                 $tablica.add("ComputerSerialNumber",$NULL)
                 $tablica.add("ProcessorName",       $Win32_Processor.Name)
+                $tablica.add("NrOfProcessors",      ($Win32_Processor | Measure-Object | Select-Object -ExpandProperty Count))
                 $tablica.add("RAM",                 ([math]::Round(($Win32_ComputerSystem.TotalPhysicalMemory / 1GB),2)))
                 $tablica.add("DiskDrive",           ($Win32_DiskDrive.Size | ForEach-Object {[math]::Round(($_ / 1GB),2)}) )
-                #$tablica.add("HDDPartitions",       $NULL)
+                #$tablica.add("HDDPartitions",      $NULL)
                 $tablica.add("OS",                  $OS)   
-                $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor) + "." + ($PSVersionTable.PSVersion.Patch)))
+                $tablica.add("LocalAdministrators", $NULL)
+                $tablica.add("IPAddress",           $NULL)
+                $tablica.add("SuffixOrigin",        $NULL)
+                $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor)))
                 
 
                 new-object -Type PSobject -Property $tablica
@@ -139,18 +150,22 @@ if ($TRUE -eq $computersToScan) {
 
 
         # Saving results to a file
-        $results | select-object -property `
-                                "ScanDate", `
-                                "PSComputerName", `
-                                #"LogicalHostname", `
-                                "PhysicalHostname", `
-                                "Manufacturer", `
-                                "Model", `
-                                "ProcessorName", `
-                                "RAM" , `
-                                "DiskDrive", `
-                                "OS", `
-                                "PSVersion" `
+        $results | select-object -property              `
+                                "ScanDate",             `
+                                "PSComputerName",       `
+                                #"LogicalHostname",     `
+                                "PhysicalHostname",     `
+                                "Manufacturer",         `
+                                "Model",                `
+                                "ProcessorName",        `
+                                "NrOfProcessors",       `
+                                "RAM" ,                 `
+                                "DiskDrive",            `
+                                "OS",                   `
+                                "LocalAdministrators",  `
+                                "IPAddress",            `
+                                "SuffixOrigin",         `
+                                "PSVersion"             `
                                 | Export-Csv -Path $resultsFile -Delimiter ";" -Append 
 
         Remove-Variable results
@@ -158,7 +173,7 @@ if ($TRUE -eq $computersToScan) {
     }
 
     else {
-        Write-Warning -Message "Those computers wasn't scanned before (they're not present in file) and now script couldn't establish connection with them." 
+        Write-Warning -Message "Computers listed below wasn't scanned before (they're not present in file `"$resultsFile`") and now script couldn't establish connection with them. They are offline or those are AD computer accounts of computers that was removed from use without disconnecting them from Active Directory domain." 
         $computersToScan | Sort-Object
         # to trzeba zmienić bo działa tylko wtedy gdy nie uda się nawiązać żadnej sesji w IFie, a to się raczej nei będzie zdarzało
     }
@@ -175,12 +190,22 @@ else {
 
 
 #region TRASH
+
+#GetLocalAdministrators
+#Get-LocalGroupMember -SID S-1-5-32-544 | select-object @{Name='LocalAdministrators';Expression={($_.Name).split('\')[-1]}} | Get-LocalUser -name {$_.LocalAdministrators} | select-object @{Name='LocalAdministrators';Expression={if ($TRUE -eq $_.Enabled ){$_.ToString() + "`(!`)"} else {$_.ToString() + "`(-`)"}}}
+
+
+#GetLocalAccounts from PS < 5
+#$adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
+#$adsi.Children | where {$_.SchemaClassName -eq 'user'} | Foreach-Object {$groups = $_.Groups() | Foreach-Object {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}$_}
+
 #WYCIĄGNĄĆ ZE ZMIENNEJ $_ JAKIEŚ INFORMACJE I SPERSONALIZOWAĆ KOMUNIKAT BŁĘDU. PATRZ https://youtu.be/A6afjA5Q9eM?t=1240
 #Write-Error -Message "Nie dziala dostep do AD, albo poswiadczenia sa bledne. Informacje systemowe: $Error[0]"
 
 
 # Get-NetIPAddress | Where-Object {($_."InterfaceAlias" -like "Ethernet") -and ($_."AddressFamily" -like "*IPv4*" )} | Select-Object -ExpandProperty IPAddress
 # Get-NetIPAddress -AddressState Preferred -AddressFamily IPv4 | select IPAddress,InterfaceAlias
+# (Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {$_}
 
 #Get-CimClass -Namespace root/CIMV2 | Where-Object CimClassName -like Win32* | Select-Object CimClassName -wyświetla wszystkie klasy CIM z których można pobierać dane
 
