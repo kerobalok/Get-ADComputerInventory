7e030cd2231a593b353373a0df0df5f58b505ddf
Daniel Sajdyk <kontakt@sajdyk.pl>
2020-11-22 17:07:29 +0100

    Pierwsza wersja finalna gotowa do przeslania na GitHub. Wszystko dziala. Teraz kolejnymi krokami bedzie jakas optymalizacja, ale to juz na spokojnie.

--- Get-ADComputerInventory.ps1
+++ Get-ADComputerInventory.ps1
@@ -1,22 +1,22 @@
 #region Documentation
 <#
 .Synopsis
-Ble ble ble
+The script parses computer accounts from specified Organization Unit in AD for their inventory (processor, disks, ram, OS, etc.) and saves results to csv file.
 .Description
-This script reads computer accounts with MS Windows OS from Active Directory Organization Unit and scan them for processor, OS Version, RAM etc.
+SCRIPT SHOULD BE RUN WITHIN ACCOUNT THAT HAVE PRIVILEGES TO CONNECT WITH COMPUTERS THAT SHOULD BE SCANNED.
+The script reads computer accounts with MS Windows OS from specified Active Directory Organization Unit and scan them for processor, OS Version, RAM etc. Scan results is saved to csv file which then can be parsed as a simple database using "import-csv" cmdlet. 
+For example you can use below command to show only computer names, number of installed processors, their names, and installed RAM.
+import-csv -path .\scannedComputers.csv -Delimiter ";" | select-object -property PSComputerName, ProcessorName,NrOfProcessors, RAM
 .Notes
 AUTHOR: Daniel Sajdyk
-
-
 .Link
-http://
-#Requires-Version ??????
+http://https://github.com/kerobalok/Get-ADComputerInventory
 #>
 #endregion
 
 #region Variables
-$OUPath = "OU=5039,OU=Rejon,OU=Resort,DC=ad,DC=ms,DC=gov,DC=pl"
-#$computersFromOU = ("5039-marnow-k.ad.ms.gov.pl","slebd01.ad.ms.gov.pl")
+# CHANGE BELOW VARIABLE $OUPath WITH PATH TO YOUR ORGANIZATION UNIT OF ACTIVE DIRECTORY WHERE YOU HAVE COMPUTER ACCOUNTS
+$OUPath = "OU=Computers,DC=domain_name,DC=gov,DC=pl"
 $resultsFile = "scannedComputers.csv"
 #endregion
 
@@ -37,7 +37,7 @@ catch {
 #endregion
 
 
-#region Checking if file with reults from previous scans exist and if so, skipping computers included in that file (they were already scanned).
+#region Checking if file with reults from previous scans exist and if so, skipping computers included in that file (because they were already scanned).
 $computersToScan = @()
 if ($TRUE -eq (Test-Path -LiteralPath $resultsFile)){
     $alreadyScannedComputers = Import-Csv -Path $resultsFile -delimiter ";"
@@ -55,8 +55,7 @@ Remove-Variable alreadyScannedComputers
 
 
 #region Scanning section
-# if variable $computersToScan exists, it means that there are no scan results in file $resultsFile and that means, that those computers wasn't scanned before
-#      ......... zrobić sprawdzanie nie czy istnieje zmienna computersToScan, ale czy istnieje i czy jest typu... ustalić jakiego typu.......
+# if variable $computersToScan exists, it means that computers included in that variable wasn't scanned before.
 if ($TRUE -eq $computersToScan) { 
     $sessions = New-PSSession -ComputerName $computersToScan -ErrorAction SilentlyContinue -Credential $credentials
 
@@ -65,14 +64,15 @@ if ($TRUE -eq $computersToScan) {
 
             # Code for PowerShell with major version greater than 5
             if (($PSVersionTable.PSVersion.Major) -ge 5) {
+                # Preparing some more complicated variables
                 $Win32_ComputerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -property *
                 $Win32_Processor = Get-CimInstance -ClassName Win32_Processor -property *
                 $Win32_SystemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure -property *
                 $Win32_DiskDrive = Get-CimInstance -ClassName Win32_DiskDrive -property *
                 $OS = ((Get-CimInstance Win32_OperatingSystem -Property *).Name.Split("|") | Select-Object -First 1)
-                #$IPAddress = (Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {($_)} | Where-Object {$_.AddressFamily -like "IPv4"};
                 $IPAddress = ((Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {($_)} | Where-Object {$_.AddressFamily -like "IPv4"} | foreach-object {"(" + ($_.SuffixOrigin) + ": " + (($_.IPAddress).ToString()) + ": " + ((Get-NetAdapter -InterfaceIndex $_.InterfaceIndex).MacAddress) + ")"})
 
+                # Creating hashtable from already existed variables and some variables created dynamically within creating hashtable
                 $tablica = @{}
                 $tablica.add("ScanDate",             (Get-Date -DisplayHint Date)) #Get-Date -Format {dd.MM.yyyy}
                 $tablica.add("PhysicalHostname",     ([System.Net.Dns]::GetHostByName($env:computerName)).HOSTNAME) #Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property {$_.Name + "." + $_.Domain}
@@ -90,7 +90,6 @@ if ($TRUE -eq $computersToScan) {
                 $tablica.add("LocalAdministrators",  (Get-LocalGroupMember -SID S-1-5-32-544))
                 $tablica.add("IPaddress",             $IPAddress)
                 #$tablica.add("IPAddress",            $IPAddress.IPAddress)
-                #$tablica.add("SuffixOrigin",         $IPAddress.SuffixOrigin)
                 $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor)))
                 
                 new-object -Type psobject -Property $tablica
@@ -98,11 +97,13 @@ if ($TRUE -eq $computersToScan) {
 
             # Code for PowerShell with major version less than 5
             else {
+                # Preparing some more complicated variables
                 $Win32_ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem
                 $Win32_Processor = Get-WmiObject -Class Win32_Processor
                 $Win32_DiskDrive = Get-WmiObject -Class Win32_DiskDrive
                 $OS = &{$x = get-wmiobject win32_operatingsystem | Select-Object -ExpandProperty Name; $x.split("|")[0]}
 
+                # Creating hashtable from already existed variables and some variables created dynamically within creating hashtable
                 $tablica = @{}
                 $tablica.add("ScanDate",            (Get-Date -DisplayHint Date))
                 $tablica.add("PhysicalHostname",    ([System.Net.Dns]::GetHostByName($env:computerName)).HOSTNAME) #($env:computername + "." + $env:USERDNSDOMAIN);
@@ -117,7 +118,6 @@ if ($TRUE -eq $computersToScan) {
                 $tablica.add("OS",                  $OS)   
                 $tablica.add("LocalAdministrators", $NULL)
                 $tablica.add("IPAddress",           $NULL)
-                #$tablica.add("SuffixOrigin",        $NULL)
                 $tablica.add("PSVersion",           (($PSVersionTable.PSVersion.Major).ToString() + "." + ($PSVersionTable.PSVersion.Minor)))
                 
 
@@ -125,8 +125,6 @@ if ($TRUE -eq $computersToScan) {
             }
         }
     
-                
-
         # Saving results to a file
         $results | select-object -property              `
                                 "ScanDate",             `
@@ -142,7 +140,6 @@ if ($TRUE -eq $computersToScan) {
                                 "OS",                   `
                                 "LocalAdministrators",  `
                                 "IPAddress",            `
-                                #"SuffixOrigin",         `
                                 "PSVersion"             `
                                 | Export-Csv -Path $resultsFile -Delimiter ";" -Append 
 
@@ -152,10 +149,7 @@ if ($TRUE -eq $computersToScan) {
 
     else {
         Write-Warning -Message "Computers listed below wasn't scanned before (they're not present in file `"$resultsFile`") and now script couldn't establish connection with them. They are offline or those are accounts of computers removed without proper disconnecting them from Active Directory domain." 
-        #$computersToScan | Sort-Object
-        #Get-ADComputer -SearchBase $OUPath -Filter * -property * | select-object -property Name, @{Name="LastLogon";Expression={[datetime]::FromFileTime($_.LastLogonTimeStamp)}}
-        $computersToScan | ForEach-Object {Get-ADComputer -Filter "DNSHostName -eq '$_'" -Properties LastLogonDate | select-object DNSHostName, LastLogonDate}
-        # to trzeba zmienić bo działa tylko wtedy gdy nie uda się nawiązać żadnej sesji w IFie, a to się raczej nei będzie zdarzało
+        $computersToScan | ForEach-Object {Get-ADComputer -Credential $credentials -Filter "DNSHostName -eq '$_'" -Properties LastLogonDate | select-object DNSHostName, LastLogonDate}
     }
     
     Remove-Variable computersToScan
@@ -164,40 +158,21 @@ if ($TRUE -eq $computersToScan) {
 else {
     write-host "All computers from $OUPath was already scanned."
 }
-
 #endregion
 
 
 
 #region TRASH
-
 #GetLocalAdministrators
 #Get-LocalGroupMember -SID S-1-5-32-544 | select-object @{Name='LocalAdministrators';Expression={($_.Name).split('\')[-1]}} | Get-LocalUser -name {$_.LocalAdministrators} | select-object @{Name='LocalAdministrators';Expression={if ($TRUE -eq $_.Enabled ){$_.ToString() + "`(!`)"} else {$_.ToString() + "`(-`)"}}}
-
 
 #GetLocalAccounts from PS < 5
 #$adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
 #$adsi.Children | where {$_.SchemaClassName -eq 'user'} | Foreach-Object {$groups = $_.Groups() | Foreach-Object {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}$_}
 
-#WYCIĄGNĄĆ ZE ZMIENNEJ $_ JAKIEŚ INFORMACJE I SPERSONALIZOWAĆ KOMUNIKAT BŁĘDU. PATRZ https://youtu.be/A6afjA5Q9eM?t=1240
-#Write-Error -Message "Nie dziala dostep do AD, albo poswiadczenia sa bledne. Informacje systemowe: $Error[0]"
-
-
-# Get-NetIPAddress | Where-Object {($_."InterfaceAlias" -like "Ethernet") -and ($_."AddressFamily" -like "*IPv4*" )} | Select-Object -ExpandProperty IPAddress
-# Get-NetIPAddress -AddressState Preferred -AddressFamily IPv4 | select IPAddress,InterfaceAlias
-#  $i = 1; (Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {($_)} | Where-Object {$_.AddressFamily -like "IPv4"} | foreach-object {($i).ToString() + "." + (($_.IPAddress).ToString()); $i++}
-# ((Get-NetConnectionProfile).InterfaceIndex | Get-NetIPAddress -InterfaceIndex {($_)} | Where-Object {$_.AddressFamily -like "IPv4"} | foreach-object {"(" + ($_.SuffixOrigin) + ": " + (($_.IPAddress).ToString()) + ": " + ((Get-NetAdapter -InterfaceIndex $_.InterfaceIndex).MacAddress) + ")"})
-
 #Get-CimClass -Namespace root/CIMV2 | Where-Object CimClassName -like Win32* | Select-Object CimClassName -wyświetla wszystkie klasy CIM z których można pobierać dane
 
-#$eeee | Select-Object -Property Producent, Model, PSComputerName, OS, ProcesorName, RAM, HDD
-#$e.HDD.ForEach({[math]::Round($_ / 1GB,2)})
-#$sessions | Remove-PSSession
 #https://community.spiceworks.com/topic/932043-powershell-computer-age
 #https://stackoverflow.com/questions/56980098/get-computer-manufacture-date-by-evaluate-cpu-dates-from-a-csv-file-using-powers
-
-
-
-#Get-ADComputer -SearchBase "OU=5039,OU=Rejon,OU=Resort,DC=ad,DC=ms,DC=gov,DC=pl" -Filter * -property * | select-object -property Name, @{Name="LastLogon";Expression={[datetime]::FromFileTime($_.LastLogonTimeStamp)}} | Sort-Object -Property LastLogon
 
 #endregion
